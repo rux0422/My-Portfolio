c7f07d0497cc33fa6060129d598c471c7c56ae21
rux0422 <109064159+rux0422@users.noreply.github.com>
2025-12-07 19:25:44 -0600

    Update title for EcoCoin project in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,7 +52,7 @@ const projects = [
     href: "https://ruxquerybuddy.streamlit.app/"
   },
   {
-    title: "EcoCoin - An Incentivized Method to Go Green using Amazon Rekognition (IEEE TENCON 24')",
+    title: "EcoCoin: A Mobile App for Sustainable Actions (IEEE TENCON 24')",
     description:
       "A college student app (prototype) promoting sustainability using AWS services. The app rewards students for eco-friendly actions with redeemable coins. The app utilizes AWS cloud services for secure facial recognition from the photo they upload in the portal and the video of the sustainable activity they perform to ensure authenticity in rewarding eco-friendly activities (github code below).",
     githubhref: "https://github.com/rux0422/EcoCoin/tree/main/ecocoin/EcoCoin",
