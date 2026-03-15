a2fa95dedcb978e0bd918a44797ed88cc7d2ab19
rux0422 <109064159+rux0422@users.noreply.github.com>
2026-03-15 17:18:29 -0500

    Update resume download link in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -356,7 +356,7 @@ export default function Home() {
               data-scroll-speed=".06"
               className="flex flex-row items-center space-x-1.5 pt-6"
             >
-              <Button onClick={() => window.open("https://drive.google.com/file/d/1kUZf5yfiYgTpGd5yIPvoO5QDCQCRcKQM/view?usp=sharing")}>
+              <Button onClick={() => window.open("https://drive.google.com/file/d/1DZeC4Fr3H4J-kM43pwUbz9eIJy0hF0Ck/view?usp=sharing")}>
                 Download Resume <Download className="ml-1 h-4 w-4" />
               </Button>
             </span>
